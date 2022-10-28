d3cbb8732a743908bfe63c55a15a03b1804bfb7f
UmeeAndrey <andrey@umee.cc>
2022-10-28 12:03:14 -0700

    OMG IT WORKED!

--- asset-updater/index.js
+++ asset-updater/index.js
@@ -1,6 +1,6 @@
 const util = require('util')
 const path = require('path')
-const { readdir, readFile, exists  } = require('fs')
+const { readdir, readFile, exists } = require('fs')
 const { Firestore } = require('@google-cloud/firestore');
 const ASSETS_DIR = '../assets'
 const PROJECTS_DIR = '../projects'
@@ -12,7 +12,7 @@ async function extractDirectoryContents(TARGET_DIR) {
   console.log('EXTRACTING FROM', TARGET_DIR)
   const dirExists = await util.promisify(exists)(TARGET_DIR)
 
-  if(!dirExists){
+  if (!dirExists) {
     console.log(`${TARGET_DIR} does not exists in repository ... aborting`)
     return
   }
@@ -37,26 +37,26 @@ async function extractDirectoryContents(TARGET_DIR) {
 
 }
 async function storeAssets(assets) {
-  if(!assets || !assets.length > 0){
+  if (!assets || !assets.length > 0) {
     return
   }
   try {
-   console.log('SAVING ASSETS', assets)
+    console.log('SAVING ASSETS', assets)
     firestore.runTransaction(async t => {
       const snapshot = await firestore.collection('assets').get()
       console.log(`SNAPSHOT`, snapshot)
-      if(snapshot.exists){
+      if (snapshot.exists) {
         const updateSymbolList = assets.map(asset => asset.symbol)
 
         const docData = snapshot.docs.map(doc => doc.data());
-  
+
         const deleteAssetList = docData.reduce((acc, doc) => {
           if (!updateSymbolList.includes(doc.symbol)) {
             acc.push(doc.symbol)
           }
           return acc
         }, [])
-  
+
         await Promise.all(deleteAssetList.map(delAssetSymbol => {
           firestore.collection('assets')
             .doc(delAssetSymbol)
@@ -80,46 +80,49 @@ async function storeAssets(assets) {
   }
 
 }
-// async function storeProjects(projects) {
-//   if(!projects || projects.length < 1){
-//     return
-//   }
-//  console.log('SAVING PROJECTS', projects)
-//   try {
-//     firestore.runTransaction(async t => {
-//       const snapshot = await firestore.collection('projects').get()
-//       const updateSymbolList = projects.map(project => project.name)
+async function storeProjects(projects) {
+  if (!projects || projects.length < 1) {
+    return
+  }
+  console.log('SAVING PROJECTS', projects)
+  try {
+    firestore.runTransaction(async t => {
+      const snapshot = await firestore.collection('projects').get()
+      if (snapshot.exists) {
+        const updateSymbolList = projects.map(project => project.name)
 
-//       const docData = snapshot.docs.map(doc => doc.data());
+        const docData = snapshot.docs.map(doc => doc.data());
 
-//       const deleteAssetList = docData.reduce((acc, doc) => {
-//         if (!updateSymbolList.includes(doc.name)) {
-//           acc.push(doc.name)
-//         }
-//         return acc
-//       }, [])
+        const deleteAssetList = docData.reduce((acc, doc) => {
+          if (!updateSymbolList.includes(doc.name)) {
+            acc.push(doc.name)
+          }
+          return acc
+        }, [])
+      }
 
-//       await Promise.all(deleteAssetList.map(delAssetSymbol => {
-//         firestore.collection('projects')
-//           .doc(delAssetSymbol)
-//           .delete()
-//       }))
 
-//       await Promise.all(assets.map(async asset => {
-//         const key = asset.name;
-//         firestore.collection('projects')
-//           .doc(key)
-//           .set(asset)
-//       }))
+      await Promise.all(deleteAssetList.map(delAssetSymbol => {
+        firestore.collection('projects')
+          .doc(delAssetSymbol)
+          .delete()
+      }))
 
-//       return t
-//     })
+      await Promise.all(assets.map(async asset => {
+        const key = asset.name;
+        firestore.collection('projects')
+          .doc(key)
+          .set(asset)
+      }))
 
-//   } catch (e) {
-//     console.log(`Transaction failure`, e)
-//   }
-// }
+      return t
+    })
+
+  } catch (e) {
+    console.log(`Transaction failure`, e)
+  }
+}
 
 console.log('STARGING ASSET AND PROJECT UPDATER')
 extractDirectoryContents(ASSETS_DIR).then(assets => storeAssets(assets));
-// extractDirectoryContents(PROJECTS_DIR).then(projects => storeProjects(projects));
+extractDirectoryContents(PROJECTS_DIR).then(projects => storeProjects(projects));
